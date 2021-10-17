ad2957f0af43c02c36659de1fd3a941e3933295e
Denis_P <kerityryui@mail.ru>
2021-10-17 16:57:20 +0300

    Start to write code for creating map.

---
+++ src/main/java/BattleShips.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class BattleShips {
+    public static int numRows = 10;
+    public static int numCols = 10;
+    public static int playerShips = 0;
+    public static int computerShips = 0;
+    public static String[][] grid = new String[numRows][numCols];
+    public static int[][] missedGuesses = new int[numRows][numCols];
+
+    public static void main(String[] args){
+        System.out.println("===> Welcome to the Game <===");
+        System.out.println("At that time sea is empty.\n");
+
+        //Creating map for playing
+        createOceanMap();
+    }
+
+    //Fill grid and show it
+    public static void createOceanMap(){
+        //First line
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+
+        //Middle lines
+        for(int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = " ";
+                if (j == 0)
+                    System.out.print(i + "|" + grid[i][j]);
+                else if (j == grid[i].length - 1)
+                    System.out.print(grid[i][j] + "|" + i);
+                else
+                    System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+
+        //Last line
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+}
